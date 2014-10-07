085d169befad04c941810fb60240b3cdda16f936
oesgalha <oscaresgalha@gmail.com>
2014-10-06 22:02:58 -0300

    Fix do problema de permissão

--- servidor.c
+++ servidor.c
@@ -26,7 +26,7 @@ int main (int argc, char **argv) {
    bzero(&servaddr, sizeof(servaddr));
    servaddr.sin_family      = AF_INET;
    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-   servaddr.sin_port        = htons(13);
+   servaddr.sin_port        = htons(1024);
 
    if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
       perror("bind");
